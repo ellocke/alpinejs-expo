6f0aa72241ba27d11c1526e6fb69559f08f09bd9
fubits <ilja_s@protonmail.com>
2022-05-22 01:08:39 +0200

    cleanup; darkmode css example

--- js/helpers.js
+++ js/helpers.js
@@ -13,3 +13,23 @@ function reverseString(str) {
 //     })
 //   return books
 // }
+
+
+// Mocks SPA-like page routing
+// sourcing local <script> requires addition workaround
+// document.addEventListener("click", (event) => {
+//   if (event.target.tagName === 'A' && event.target.origin === location.origin) {
+//     console.log("Mock SPA Page Change");
+//     event.preventDefault();
+//     const path = event.target.pathname
+//     fetch(event.target.href).then(response => response.text()).then(payload => {
+//       window.history.pushState(path, path, path);
+//       const newNode = new DOMParser().parseFromString(payload, "text/html").querySelector("div.container");
+//       console.log(newNode.classList);
+//       const rootNode = document.querySelector("div.container");
+//       console.log(rootNode);
+//       document.body.replaceChild(newNode, rootNode);
+//       window.scrollTo(0, 0)
+//     })
+//   }
+// })
